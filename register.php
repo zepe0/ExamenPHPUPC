425df18491b4b5879b60578a87cf9da3c302e8ad
zepe0 <aitor.zepe@gmail.com>
2024-05-13 09:20:03 +0200

    add register

---
+++ register.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Prova</title>
+</head>
+
+<body>
+    <?php
+    session_start();
+
+    $errorUser;
+    $errorPass;
+    $errorPassdos;
+    $errorEmail;
+    $error = false;
+    include_once ("header.php");
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        if (isset($_POST["User"])) {
+            if (strlen($_POST["User"]) < 8) {
+
+                $errorUser = "El nombre de usuario debe contener 8 caracteres";
+                $error = true;
+            } else {
+                $errorUser = false;
+            }
+        }
+        ;
+        if (isset($_POST["Pass"])) {
+            $regexpas = '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\W).{8,}$/';
+
+
+            if (strlen($_POST["Pass"]) < 8) {
+                $errorPass = "La Contraseña debe contener 8 caracteres mínimo";
+                $error = true;
+
+            } else {
+                $errorPass = false;
+            }
+            if ($errorPass === false) {
+
+                if (!preg_match($regexpas, $_POST["Pass"])) {
+                    $errorPass = "La Contraseña debe contener al menos una mayúscula, una minúscula y un caracter especial";
+                    $error = true;
+                } else {
+                    $errorPass = false;
+                }
+            }
+
+
+        }
+        ;
+        if (isset($_POST["Passdos"])) {
+            if ($_POST["Passdos"] != $_POST["Pass"]) {
+                $errorPassdos = "Las Contraseñas no coinciden";
+
+
+            }
+            if (empty($_POST["Passdos"])) {
+                $errorPassdos = "Rellena campo";
+                $error = true;
+            } else {
+                $errorPassdos = false;
+            }
+
+        }
+        ;
+        if (strlen($_POST["email"]) === 0) {
+            $error = true;
+            $errorEmail = "Email incorrecto";
+        }
+        if (isset($_POST["email"])) {
+            $email = $_POST["email"];
+
+            $regex = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+
+            if (!preg_match($regex, $email)) {
+                $error = true;
+                $errorEmail = "Email incorrecto";
+            } else {
+                $error = false;
+            }
+        }
+
+        ;
+        if ($error === false) {
+            header("Location:home.php");
+            $_SESSION["login"] = 1;
+        }
+
+
+
+    } ?>
+    <form method="POST" class="register">
+        <input type="text" placeholder="Usuario" name="User">
+        <?php echo isset($errorUser) ? "<spam class='heart'>" . $errorUser . "</spam>" : ""; ?>
+        <input type="password" placeholder="Contraseña" name="Pass">
+        <?php echo isset($errorPass) ? "<spam class='heart'>" . $errorPass . "</spam>" : ""; ?>
+        <input type="password" placeholder="Repite Contraseña" name="Passdos">
+        <?php echo isset($errorPassdos) ? "<spam class='heart'>" . $errorPassdos . "</spam>" : ""; ?>
+        <input type="email" placeholder="Email" name="email">
+        <?php echo isset($errorEmail) ? "<spam class='heart'>" . $errorEmail . "</spam>" : ""; ?>
+
+        <button name="action">Enviar</button>
+        <a href="index.php">Ya tienes Cuenta ➡ </a>
+    </form>
+
+    <?php include_once ("footer.php");
+    ?>
+</body>
+
+</html>
